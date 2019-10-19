6214a80d2792f1014f5f9dd43ae1fb34722642d0
Warren Yao <warrenyca905@gmail.com>
2019-10-19 21:01:13 +0000

    building out the expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,59 +3,59 @@ class ExpensesController < ApplicationController
     if logged_in?
       @user = current_user
       @balance = @user.balance
-      erb :"/tweets/tweets"
+      erb :"/expenses/show_expense"
     else
-      redirect "/login"
+      redirect "/"
     end
   end
   
-  get '/tweets/new' do
+  get '/expense/new' do
     if logged_in?
-      erb :"/tweets/new"
+      erb :"/expenses/new"
     else
-      redirect "/login"
+      redirect "/"
     end
   end
   
-  post '/tweets/new' do
+  post '/expense/new' do
     if params[:content] != ""
-      new_tweet = Tweet.create(content: params[:content])
-      current_user.tweets << new_tweet
+      new_expense = Expense.create(content: params[:content])
+      current_user.expenses << new_expense
     else
-      redirect "/tweets/new"
+      redirect "/balance"
     end
   end
   
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :"/tweets/show_tweet"
-    else
-      redirect "/login"
-    end
-  end
+  get '/expenses/:id' do
+  #   if logged_in?
+  #     @user = current_user
+  #     erb :"/users/show_expense"
+  #   else
+  #     redirect "/login"
+  #   end
+  # end
   
-  get '/tweets/:id/edit' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :"/tweets/edit_tweet"
-    else
-      redirect "/login"
-    end
-  end
+  # get '/tweets/:id/edit' do
+  #   if logged_in?
+  #     @tweet = Tweet.find(params[:id])
+  #     erb :"/tweets/edit_tweet"
+  #   else
+  #     redirect "/login"
+  #   end
+  # end
   
-  patch '/tweets/:id/edit' do
-    tweet = Tweet.find(params[:id])
-    tweet.content = params[:content]
-    tweet.save
-  end
+  # patch '/tweets/:id/edit' do
+  #   tweet = Tweet.find(params[:id])
+  #   tweet.content = params[:content]
+  #   tweet.save
+  # end
   
-  delete '/tweets/:id/delete' do
-    tweet = Tweet.find(params[:id])
-    if tweet.user_id == current_user.id
-      tweet.delete
-      redirect "/tweets"
-    end
-    redirect "/tweets"
-  end
+  # delete '/tweets/:id/delete' do
+  #   tweet = Tweet.find(params[:id])
+  #   if tweet.user_id == current_user.id
+  #     tweet.delete
+  #     redirect "/tweets"
+  #   end
+  #   redirect "/tweets"
+  # end
 end
